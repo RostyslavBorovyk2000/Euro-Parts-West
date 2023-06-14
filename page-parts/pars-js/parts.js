cf5265dade90f8d0db3c9a97471670189ba61083
Rostyslav <rostykfox911@gmail.com>
2023-06-15 00:13:25 +0300

    fixed block

--- page-parts/pars-js/parts.js
+++ page-parts/pars-js/parts.js
@@ -14,12 +14,12 @@ btnDropDown.addEventListener("click", () => {
 // Форма
 
 const form = document.querySelector(".sign-up");
-const btnForm = document.querySelector(".btn_parts");
+const btnForm = document.querySelector(".block_inform_parts-btn");
 const closeForm = document.querySelector(".close-img");
 const send = document.querySelector(".wrapper-send");
 const sendAccept = document.querySelector(".accept-sign-up");
 const closeAccept = document.querySelector(".accept-close-img");
-const btnWidht = document.querySelector(".btn_parts-js-btn");
+const btnWidht = document.querySelector(".block_inform_parts-btn-two");
 
 btnWidht.addEventListener("click", () => {
   form.style.display = "flex";
@@ -43,50 +43,46 @@ closeAccept.addEventListener("click", () => {
 
 // Галерея запчастин
 
-const cards = document.querySelector(".cards");
-const blockInput = document.querySelector(".block");
-const wrapperInform = document.querySelector(".wrapper_parts");
-
-const btnOne = document.querySelector(".js-one");
-const btnTwo = document.querySelector(".js-two");
-const btnThree = document.querySelector(".js-three");
-const btnFour = document.querySelector(".js-four");
-const btnSix = document.querySelector(".js-six");
-const btnSeven = document.querySelector(".js-seven");
-const btnEight = document.querySelector(".js-eight");
-const btnNine = document.querySelector(".js-nine");
-const btnTen = document.querySelector(".js-ten");
-const btnEleven = document.querySelector(".js-eleven");
-const btnTwelv = document.querySelector(".js-twelv");
-const btnThirteen = document.querySelector(".js-thirteen");
-const btnFourteen = document.querySelector(".js-fourteen");
-const btnFifteen = document.querySelector(".js-fifteen");
-const btnSixteen = document.querySelector(".js-sixteen");
-
-class Card {
-  constructor(img, name, number) {
-    this.img = img;
-    this.name = name;
-    this.number = number;
+class Categories {
+  constructor(link) {
+    this.link = link;
   }
 
   render() {
-    cards.insertAdjacentHTML(
+    const list = document.querySelector(".list_categoriys");
+
+    list.insertAdjacentHTML(
       "beforeend",
-      ` 
-      <div class="card">
-        <img src="${this.img}" alt="Запчастина">
-        <h3 class="name-part">${this.name}</h3>
-        <p class="number-part">${this.number}</p>
-        <button class="btn-card">Замовити</button>
-      </div>      
-    `
+      ` <li class="item_categoriys">Двигун</li>
+    <li class="item_categoriys">КПП</li>
+    <li class="item_categoriys">Кутовий редуктор</li>
+    <li class="item_categoriys">Мости</li>
+    <li class="item_categoriys">Стріла</li>
+    <li class="item_categoriys">Електросистема</li>
+    <li class="item_categoriys">Паливна система</li>
+    <li class="item_categoriys">Гідравлічна система</li>
+    <li class="item_categoriys">Запчастини для ТО</li>
+    <li class="item_categoriys">Інші запчастини</li> `
     );
   }
 }
 
-btnOne.addEventListener("click", () => {
-  wrapperInform.style.display = "none";
-  blockInput.style.display = "block";
-  new Card("rostyk", "borovyk", 122323).render();
+window.addEventListener("load", () => {
+  new Categories("link").render();
+
+  const li = document.querySelectorAll(".item_categoriys");
+
+  li.forEach(element => {
+    element.addEventListener("click", () => {});
+  });
 });
+
+class Card {
+  constructor(name, artNumber, linkImg) {
+    this.name = name;
+    this.artNumber = artNumber;
+    this.linkImg = linkImg;
+  }
+
+  creatElement() {}
+}
