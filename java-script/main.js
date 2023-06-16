4bfcd3c3a0f14c4e5601b939bd3dcc551c8442e2
Rostyslav <rostykfox911@gmail.com>
2023-06-17 01:09:57 +0300

    fixed img

--- java-script/main.js
+++ java-script/main.js
@@ -1,7 +1,6 @@
 import burger from "./bar-menu.js";
 import forms from "./form.js";
 import sliderHeader from "./slider-header.js";
-import aboutAs from "./slider-about-us.js";
 import sliderService from "./slider-servise.js";
 
 // Запускаємо імпортовану функцію для БАР МЕНЮ;
@@ -13,8 +12,20 @@ forms();
 //Слайдер секції header
 sliderHeader();
 
-// Слайдер блоку про нас
-aboutAs();
-
 // Слайдер для секції service при розмірі 320px-768px
 sliderService;
+
+const img = document.querySelector(".block_inform-images");
+const largeScreenImagePath = "images/header/header-nav-img-one.jpg";
+const smallScreenImagePath = "images/header/header-nav-img-width-max.jpg";
+
+function updateImagePath() {
+  if (window.innerWidth > 1024) {
+    img.src = largeScreenImagePath;
+  } else {
+    img.src = smallScreenImagePath;
+  }
+}
+
+updateImagePath();
+window.addEventListener("resize", updateImagePath);
