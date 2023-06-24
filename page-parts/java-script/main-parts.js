b35bbf9ced3eb3be6fe88902340f79f185245ef4
Rostyslav <rostykfox911@gmail.com>
2023-06-24 14:39:39 +0300

    add function filter card

--- page-parts/java-script/main-parts.js
+++ page-parts/java-script/main-parts.js
@@ -10,28 +10,51 @@ forms();
 
 // Галерея запчастин........................................................................
 
-const mainContainer = document.querySelector(".list_categoriys");
-const categoriesContainer = document.querySelector(".all-products");
-const block = document.querySelector(".block");
-const wrapperBlockInform = document.querySelector(".js-wrapper");
-
 const getQueryParam = param => {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(param);
 };
 
-window.addEventListener("popstate", event => {
-  event.preventDefault();
-  const state = event.state;
-  if (state && state.category) {
-    const categoryQueryParam = state.category;
+const categoriesContainer = document.querySelector(".all-products");
+const input = document.querySelector(".input_seurch-card");
+
+const filterCards = () => {
+  const cards = document.querySelectorAll(".product");
+
+  cards.forEach(card => {
+    const cardTitle = card
+      .querySelector(".product-title")
+      .textContent.toLowerCase();
+    const cardArtNumber = card
+      .querySelector(".product-price")
+      .textContent.toLowerCase();
+    const inputValue = input.value.toLowerCase();
+
+    if (cardTitle.includes(inputValue) || cardArtNumber.includes(inputValue)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+};
+
+input.addEventListener("input", filterCards);
+
+const categoryQueryParam = getQueryParam("categories");
+
+const showCardsByCategory = categoryQueryParam => {
+  const wrapperBlockInform = document.querySelector(".js-wrapper");
+  const mainContainer = document.querySelector(".list_categoriys");
+  const block = document.querySelector(".block");
+
+  if (categoryQueryParam) {
     wrapperBlockInform.classList.add("hidden");
     mainContainer.classList.add("hidden");
     block.classList.remove("hidden");
+
     axios
-      .get(`http://127.0.01:8000/api/categories/${categoryQueryParam}`)
+      .get(`http://127.0.0.1:8000/api/categories/${categoryQueryParam}`)
       .then(({ data }) => {
-        console.log(data);
         if (categoryQueryParam == data.id) {
           categoriesContainer.innerHTML = "";
           const products = data.products;
@@ -39,41 +62,20 @@ window.addEventListener("popstate", event => {
             new Card(name, image_url, article).render();
           });
         }
+      })
+      .catch(error => {
+        if (error.response && error.response.status === 404) {
+          console.log("Сторінку не знайдено");
+        } else {
+          console.log("Сталася помилка при отриманні даних");
+          console.error(error);
+        }
       });
   } else {
     wrapperBlockInform.classList.remove("hidden");
     mainContainer.classList.remove("hidden");
     block.classList.add("hidden");
   }
-});
+};
 
-const categoryQueryParam = getQueryParam("categories");
-if (categoryQueryParam) {
-  wrapperBlockInform.classList.add("hidden");
-  mainContainer.classList.add("hidden");
-  block.classList.remove("hidden");
-  axios
-    .get(`http://127.0.01:8000/api/categories/${categoryQueryParam}`)
-    .then(({ data }) => {
-      console.log(data);
-      if (categoryQueryParam == data.id) {
-        categoriesContainer.innerHTML = "";
-        const products = data.products;
-        products.forEach(({ name, image_url, article }) => {
-          new Card(name, image_url, article).render();
-        });
-      }
-    })
-    .catch(error => {
-      if (error.response && error.response.status === 404) {
-        console.log("Страницю не знайдено");
-      } else {
-        console.log("Сталася помилка при отриманні даних");
-        console.error(error);
-      }
-    });
-} else {
-  wrapperBlockInform.classList.remove("hidden");
-  mainContainer.classList.remove("hidden");
-  block.classList.add("hidden");
-}
+showCardsByCategory(categoryQueryParam);
